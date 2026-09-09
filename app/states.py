b0dbd17d18067c2f492332ea588eb38ad0ead016
mirfayzfarxodov1-png <mirfayzfarxodov1@gmail.com>
2026-09-09 21:35:44 +0500

    add admin management FSM states

--- app/states.py
+++ app/states.py
@@ -1,6 +1,22 @@
 from aiogram.fsm.state import State, StatesGroup
-class AddAnime(StatesGroup): title=State()
-class Upload(StatesGroup): episodes=State(); manual_number=State()
-class Search(StatesGroup): query=State()
-class Broadcast(StatesGroup): text=State()
-class EditAnime(StatesGroup): field=State(); value=State()
+
+class AddAnime(StatesGroup):
+    title = State()
+
+class Upload(StatesGroup):
+    episodes = State()
+    manual_number = State()
+
+class Search(StatesGroup):
+    query = State()
+
+class Broadcast(StatesGroup):
+    text = State()
+
+class EditAnime(StatesGroup):
+    field = State()
+    value = State()
+
+class AdminManage(StatesGroup):
+    add_id = State()
+    remove_id = State()
